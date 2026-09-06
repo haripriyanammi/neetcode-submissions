24042ee62581fee63ab8b935e4a4804162e934e3
Hari priya <168447595+haripriyanammi@users.noreply.github.com>
2026-09-06 16:41:08 +0530

    Add: is-anagram - submission-17

---
+++ Data Structures & Algorithms/is-anagram/submission-17.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+     int[] Freq=new int[26];
+     for(char c:s.toCharArray())
+     {
+      Freq[c-'a']++;
+     }
+    for(char c:t.toCharArray())
+    {
+      Freq[c-'a']--;
+    }
+    for(int count:Freq)
+    {
+      if(count!=0)
+      {
+        return false;
+      }
+    }
+    return true;
+    }
+}
